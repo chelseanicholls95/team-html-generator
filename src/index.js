8c9d4b7a91b42417bbe89c20aee4020a02cd5cfe
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-05-09 20:40:03 +0100

    moved manager questions to seperate file

--- src/index.js
+++ src/index.js
@@ -1,35 +1,19 @@
 const addEmployees = require("./utils/addEmployees");
 const createEmployee = require("./utils/createEmployee");
+const createManager = require("./utils/createManager");
 const getAnswers = require("./utils/getAnswers");
 
-const managersQuestions = [
+const initialQuestion = [
   {
     message: "What is the name of your team?",
     name: "teamName",
   },
-  {
-    message: "What is the manager's name?",
-    name: "managerName",
-  },
-  {
-    message: "What is the manager's ID?",
-    name: "managerId",
-  },
-  {
-    message: "What is the manager's email?",
-    name: "managerEmail",
-  },
-  {
-    message: "What is the office number?",
-    name: "officeNumber",
-  },
 ];
 
 const init = async () => {
-  const managersAnswers = await getAnswers(managersQuestions);
-  managersAnswers.employeeRole = "Manager";
-  addEmployees(managersAnswers);
-  createEmployee();
+  const initialAnswer = await getAnswers(initialQuestion);
+  await createManager();
+  await createEmployee();
 };
 
 init();
