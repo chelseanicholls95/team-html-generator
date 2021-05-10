a1677da7f81c0bd215a31efdc496ec166e86ef4e
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-05-10 21:25:04 +0100

    added Engineer class and tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,42 @@
+const Engineer = require("../lib/Engineer");
+
+describe("Engineer class", () => {
+  const engineerDetails = {
+    employeeName: "Bob",
+    employeeId: "1234",
+    employeeEmail: "bob@email.com",
+    github: "bob111",
+  };
+
+  it("should set github via constructor", () => {
+    const expected = engineerDetails.github;
+    const engineer = new Engineer(engineerDetails);
+    const actual = engineer.github;
+
+    expect(actual).toBe(expected);
+  });
+
+  it("should set employeeRole via constructor", () => {
+    const expected = "Engineer";
+    const engineer = new Engineer(engineerDetails);
+    const actual = engineer.employeeRole;
+
+    expect(actual).toBe(expected);
+  });
+
+  it("should return github when getOfficeNumber is called", () => {
+    const expected = engineerDetails.github;
+    const engineer = new Engineer(engineerDetails);
+    const actual = engineer.getGithub();
+
+    expect(actual).toBe(expected);
+  });
+
+  it("should return employeeRole when getRole is called", () => {
+    const expected = "Engineer";
+    const engineer = new Engineer(engineerDetails);
+    const actual = engineer.getRole();
+
+    expect(actual).toBe(expected);
+  });
+});
